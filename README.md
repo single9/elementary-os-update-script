f1e5434726b82de7dfe1a55bd3047f7bcb4d5407
Duye Chen <k078264@gmail.com>
2013-08-17 05:49:51 +0800

    release

---
+++ README.md
@@ -0,0 +1,27 @@
+# elementary update script
+## 簡介
+
+這個 Bash shell script 是基於[Top Things To Do After Installing Luna](http://www.elementaryupdate.com/2013/08/top-things-to-do-after-installing-luna.html)這篇文章而來的，因為裡面太多指令太麻煩，我就自己動手寫了一個腳本方便安裝，不過好像直接下指令的時間比我寫這腳本時間來得快的樣子？
+
+哈哈，無論如何，希望這樣的東西能幫到使用 Elementary OS Luna 的你。
+
+這腳本含有以下這些套件的安裝語法：
+
+1. Elementary Tweak
+2. WINGPANEL SLIM
+3. INDICATOR SYNAPSE
+4. Audience
+5. 佈景主題
+6. 圖示主題
+7. 其他佈景主題
+8. 桌布集
+
+## 使用方法
+
+下載回家後，請先給他一個執行權限
+
+    chmod +x elementary_update.sh
+
+然後，執行他，並且跟著指示安裝就行了。
+
+    ./elementary_update.sh
---
+++ elementary_update.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+# Program:
+#       Automatic install some package to allow you to customize your elementary OS.
+#
+# History:
+# 2013/08/17 Duye Chen release Chinese version
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+function show_package()
+{
+    echo -e "安裝項目：\n"\
+            "1. Elementary Tweak\t協助調整系統用的工具\n"\
+            "2. WINGPANEL SLIM\t美化系統欄\n"\
+            "3. INDICATOR SYNAPSE\t取代附屬應用程式選單的好套件\n"\
+            "4. Audience\t\t與eOS高度融合的影片播放軟體\n"\
+            "5. 佈景主題\n"\
+            "6. 圖示主題\n"\
+            "7. 其他佈景主題\n"\
+            "8. 桌布集"
+}
+
+function install_choice()
+{
+    user_choice=$@
+
+    for ((i=$#; i>0; i=i-1))
+    do
+        install_package $i $user_choice;
+    done
+}
+
+function install_package()
+{
+    shift $1
+    option=$@
+
+    case $1 in
+        "1")
+            echo -e "安裝 Elementary Tweak"
+            sudo install elementary-tweaks
+            ;;
+        "2")
+            echo -e "安裝 WINGPANEL SLIM"
+            sudo apt-get install wingpanel-slim
+            ;;
+        "3")
+            echo -e "安裝 WINGPANEL SLIM"
+            sudo apt-get install indicator-synapse
+            ;;
+        "4")
+            echo -e "安裝 Audience"
+            sudo apt-get install audience
+            ;;
+        "5")
+            echo -e "安裝 佈景主題"
+            sudo apt-get install elementary-blue-theme elementary-champagne-theme elementary-colors-theme elementary-dark-theme elementary-harvey-theme elementary-lion-theme elementary-matteblack-theme elementary-milk-theme elementary-plastico-theme elementary-whit-e-theme
+            ;;
+        "6")
+            echo -e "安裝 圖示主題"
+            sudo apt-get install elementary-elfaenza-icons elementary-emod-icons elementary-enumix-utouch-icons elementary-nitrux-icons elementary-taprevival-icons
+            ;;
+        "7")
+            echo -e "安裝 其他佈景主題"
+            sudo apt-get install elementary-thirdparty-icons elementary-plank-themes
+            ;;
+        "8")
+            echo -e "安裝 桌布集"
+            sudo apt-get install elementary-wallpaper-collection
+            ;;
+    esac
+}
+
+echo -e "檢查更新...如果沒有要安裝更新請於稍候按下(n)即可."
+sudo apt-get update && sudo apt-get upgrade
+
+echo -e "檢查完畢，接下來會進行第三方應用來源的加入，這是為了安裝後續程式所需要的一個動作，若選擇否將會離開整個程式。"
+read -p "請問，是否要加入第三方應用的來源？(Y/n): " yn
+
+if [ "$yn" == "y" ] || [ "$yn" == "Y" ] || [ "$yn" == "" ]; then
+    sudo apt-add-repository ppa:versable/elementary-update
+    sudo apt-get update
+    echo -e "新增完成"
+else
+    exit
+fi
+
+echo -e "即將進行第三方應用與佈景主題等的安裝，請選擇你想要進行的動作："
+
+while [ "$option" != "1" -a "$option" != "2" -a "$option" != "3" ]
+do
+echo -e  " 1.全自動安裝\n 2.選擇要安裝的項目\n 3.離開"
+read option
+    case $option in
+        "1")
+            show_package;
+            sudo apt-get install elementary-tweaks wingpanel-slim indicator-synapse elementary-blue-theme elementary-champagne-theme elementary-colors-theme elementary-dark-theme elementary-harvey-theme elementary-lion-theme elementary-matteblack-theme elementary-milk-theme elementary-plastico-theme elementary-whit-e-theme elementary-elfaenza-icons elementary-emod-icons elementary-enumix-utouch-icons elementary-nitrux-icons elementary-taprevival-icons elementary-thirdparty-icons elementary-plank-themes elementary-wallpaper-collection audience
+           ;;
+        "2")
+            show_package;
+            echo -e "請以空白鍵當作分隔來選擇要安裝的項目。"
+            read choice
+            install_choice $choice;
+            ;;
+        "3")
+            exit
+            ;;
+         *)
+            echo -e "機會，命運請選擇...不對，你只能選123喔～\n"
+            ;;
+    esac
+done
+
+echo -e "您的選擇已經安裝完成了，如果途中有錯誤絕對不是我的問題，這樣..."
+echo -e "Elementary Tewak 會出現在系統設定中，他可以讓你調整剛剛所裝的許多內容以及你的系統"
+echo -e "祝你使用愉快～"
